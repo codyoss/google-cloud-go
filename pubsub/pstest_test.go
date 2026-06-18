2245f9dd2a439de3bd1c4fe5879d2103adc27fbc
Cody Oss <codyoss@google.com>
2026-06-18 15:01:05 +0000

    feat: stuff and things

--- pubsub/pstest_test.go
+++ pubsub/pstest_test.go
@@ -56,6 +56,7 @@ func TestPSTest(t *testing.T) {
 	}
 	defer conn.Close()
 
+	// Stuff and things
 	opts := withGRPCHeadersAssertionAlt(t, option.WithGRPCConn(conn))
 	client, err := pubsub.NewClient(ctx, "some-project", opts...)
 	if err != nil {
